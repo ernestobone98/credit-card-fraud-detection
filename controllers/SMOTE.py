741a99c027481c5ce1c7db1f00e4ced5231569c2
ernestobone98 <ernesto.bone@outlook.com>
2022-03-11 15:00:58 +0100

    SMOTE

--- controllers/SMOTE.py
+++ controllers/SMOTE.py
@@ -8,18 +8,29 @@ from collections import Counter
 
 data = pd.read_csv('models\\creditcard.csv')
 
-# Balancing data
+# showing original data
 X, y = make_classification(n_classes=2, class_sep=0.5,
     weights=[0.05, 0.95], n_informative=2, n_redundant=0, flip_y=0,
     n_features=2, n_clusters_per_class=1, n_samples=1000, random_state=10)
 
 counter = Counter(y)
 
+plt.subplot(121)
 for label, _ in counter.items():
 	row_ix = np.where(y == label)[0]
 	plt.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
-plt.legend()
-plt.show()
 
-print(counter)
-print('\n')
+
+# Over-sampling data with SMOTE
+over_sample = SMOTE()
+X_s, y_s = over_sample.fit_resample(X,y)
+
+counter_s = Counter(y_s)
+
+
+plt.subplot(122)
+for label_s, _ in counter_s.items():
+	row_ix_s = np.where(y_s == label_s)[0]
+	plt.scatter(X_s[row_ix_s, 0], X_s[row_ix_s, 1], label=str(label_s))
+
+plt.show()
