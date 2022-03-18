14842bff3207b93d692ad3fae265624662e34ea6
bbernaud <benjamin.bernaud@etu.univ-cotedazur.fr>
2022-03-18 12:12:20 +0100

    initial generating pdf file

---
+++ controllers/pdf_generator.py
@@ -0,0 +1,82 @@
+import os
+import shutil
+import numpy as np
+import pandas as pd
+import calendar
+from datetime import datetime
+from fpdf import FPDF
+import os
+
+sep = os.path.sep
+current_dir = os.getcwd()
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['axes.spines.top'] = False
+rcParams['axes.spines.right'] = False
+
+
+
+class PDF(FPDF):
+    def __init__(self):
+        super().__init__()
+        self.WIDTH = 210
+        self.HEIGHT = 297
+        
+    def header(self):
+        # Custom logo and positioning
+        # Create an `assets` folder and put any wide and short image inside
+        # Name the image `logo.png`
+        # self.image(f'{current_dir}{sep}views{sep}img{sep}logo.png', 10, 8, 33)
+        self.set_font('Arial', 'B', 11)
+        self.cell(self.WIDTH - 80)
+        self.cell(60, 1, 'Fraud report', 0, 0, 'R')
+        self.ln(20)
+        
+    def footer(self):
+        # Page numbers in the footer
+        self.set_y(-15)
+        self.set_font('Arial', 'I', 8)
+        self.set_text_color(128)
+        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
+
+    def page_body(self, images):
+        # Determine how many plots there are per page and set positions
+        # and margins accordingly
+        if len(images) == 3:
+            self.image(images[0], 15, 25, self.WIDTH - 30)
+            self.image(images[1], 15, self.WIDTH / 2 + 5, self.WIDTH - 30)
+            self.image(images[2], 15, self.WIDTH / 2 + 90, self.WIDTH - 30)
+        elif len(images) == 2:
+            self.image(images[0], 15, 25, self.WIDTH - 30)
+            self.image(images[1], 15, self.WIDTH / 2 + 5, self.WIDTH - 30)
+        else:
+            self.image(images[0], 15, 25, self.WIDTH - 30)
+            
+    def print_page(self, images):
+        # Generates the report
+        self.add_page()
+        self.page_body(images)
+
+
+def plot(data: pd.DataFrame, filename: str) -> None:
+    plt.figure(figsize=(12, 4))
+    plt.grid(color='#F2F2F2', alpha=1, zorder=0)
+    plt.plot(data[''], data[''], color='#087E8B', lw=3, zorder=5)
+    # plt.title(f'Sales 2020/{data["Date"].dt.month[0]}', fontsize=17)
+    plt.xlabel('X', fontsize=13)
+    plt.xticks(fontsize=9)
+    plt.ylabel('Y', fontsize=13)
+    plt.yticks(fontsize=9)
+    plt.savefig("figs"+sep+filename, dpi=300, bbox_inches='tight', pad_inches=0)
+    plt.close()
+    return
+
+PLOT_DIR = f'{current_dir}{sep}views{sep}reports'
+
+
+if __name__ == '__main__':
+    file = f'{current_dir}{sep}figs{sep}fig_0.png'
+    pdf = PDF()
+    pdf.print_page(file)
+    pdf.output(f'{PLOT_DIR}{sep}Report0.pdf', 'F')
