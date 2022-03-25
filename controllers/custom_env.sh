dbb0457174cc14587d1d6d76e7840668ef21b603
benj-b <benjamin@PC-Ben.localdomain>
2022-03-25 13:34:47 +0100

    update env

--- controllers/custom_env.sh
+++ controllers/custom_env.sh
@@ -1,6 +1,5 @@
 sudo apt-get install python3-venv
 python3 -m pip install --user --upgrade pip
-python3 -m venv .gitignore/env
-python3 -m pip install --force-reinstall -r controllers/requirements.txt
-
-source .gitignore/env/bin/activate
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r controllers/requirements.txt
