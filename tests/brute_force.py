a416d5d2aeaf12c2949f565550a46b6f960b8b19
mgazzera <marco.gazzera@etu.univ-cotedazur.fr>
2022-03-26 23:36:26 +0100

    Modifications du test avec Benjamin

--- tests/brute_force.py
+++ tests/brute_force.py
@@ -1,4 +1,10 @@
-liste = ["A", "a" ,"À", "à", "Â", "â", "Ä", "ä", "Ã", "ã", "B", "b", "C", "c", "ç", "D", "d", "E", "e", "é", "È", "è",
+#All of the functions we need come automatically with python but do need to be imported.
+import itertools
+import time
+
+#This is basically a list of all the different characters that will be tried.
+#You will notice many symbols missing as they were slowing down efficiency and very unlikely to appear.
+Alphabet = ["A", "a" ,"À", "à", "Â", "â", "Ä", "ä", "Ã", "ã", "B", "b", "C", "c", "ç", "D", "d", "E", "e", "é", "È", "è",
 "Ê", "ê", "Ë", "ë", "F", "f", "G", "g", "H", "h", "I", "i", "Ì", "ì", "Î", "î", "Ï", "ï", "J", "j", "K", "k", "L", "l",
 "M", "m", "N", "n", "Ñ", "ñ", "O", "o", "Ò", "ò", "Ô", "ô", "Ö", "ö", "Õ", "õ", "P", "p", "Q", "q","R", "r", "S", "s", 
 "T", "t", "U", "u", "Ù","ù", "Û", "û", "Ü", "ü", "V", "v", "W","w", "X","x", "Y", "y", "ÿ","Z", "z", "_", "-", "'", ".",
@@ -9,37 +15,73 @@ liste = ["A", "a" ,"À", "à", "Â", "â", "Ä", "ä", "Ã", "ã", "B", "b", "C"
 "ń", "Ň", "ň", "Ņ", "ņ", "Ő", "ő", "Ø", "ø", "Ŕ", "ŕ", "Ř", "ř", "Ś", "ś", "Š", "š", "Ș", "ș", "Ț", "ț", "Ť", "ť", "Ū", 
 "ū", "Ů", "ů", "Ų", "ų", "Ű", "ű", "Ź", "ź", "Ż", "ż", "Ž", "ž", "ẞ", "ß", "“", "”", "‘", "¡", "¿", ";","/", "<", ">"]
 
-mot = input("Entrer le mot de passe ")
-chaine = str()
+#This allows the user to decide what password the program will "crack" so they can decide the difficulty
+#And also so they are able to see the results a lot more reliably
+Password = input("What is your password?\n")
 
-def test(chaine,mot):
-	if chaine == mot:
-		print("Les mot de passe est: ", chaine)
+#This sets the start time so that it can be used later on in the program to calculate how long the program took.
+start = time.time()
 
-def brute_test():
-	for a in liste:
-		chaine = a
-		test(chaine,mot)
+#This tells us how many combinations are used.
+counter = 1
 
-		for b in liste:
-			chaine = a + b
-			test(chaine,mot)
+#This starts off the number of characters as 1.
+CharLength = 1
 
-			for c in liste:
-				chaine = a + b + c 
-				test(chaine,mot)
+#This stops the program once it gets to 25 characters (most people would run out of patience WAY before that
+#But if you feel the need you can increase the number.
+for CharLength in range(len(Alphabet)-1):
 
-				for d in liste:
-					chaine = a + b + c + d
-					test(chaine,mot)
+    #This finds all of the possible combinations of characters that are of the correct length.
+    passwords = (itertools.product(Alphabet, repeat = CharLength))
 
-					for e in liste:
-						chaine = a + b + c + d + e
-						test(chaine,mot)
+    #This prints three blank lines.
+    print("\n \n")
 
-						for f in liste:	
-							chaine = a + b + c + d + e + f
-							test(chaine,mot)
+    #These print information for the user on the progress of the crack.
+    print("currently working on passwords with ", CharLength, " chars")
+    print("We are currently at ", (counter / (time.time() - start)), "attempts per seconds")
+    print("It has been ", time.time() - start, " seconds!")
+    print("We have tried ", counter, " possible passwords!")
 
+    #This is the way to print the products of generators.
+    for i in passwords:
 
-brute_test()
+        #This increases the number of combinations tried by one to show that one more has been tried.
+        counter += 1
+        
+        #As the itertools.products() returns a tuple, it has to be converted into a sting.
+        i = str(i)
+
+        #The parts that were added as a result of conversion from tuple have to be removed.
+        i = i.replace("[", "")
+        i = i.replace("]", "")
+        i = i.replace("'", "")
+        i = i.replace(" ", "")
+        i = i.replace(",", "")
+        i = i.replace("(", "")
+        i = i.replace(")", "")
+
+        #This checks if the password created by the user was correct.
+        if i == Password:
+
+            #This takes the time at which the program finished.
+            end = time.time()
+
+            #This works out the time it took to find the password.
+            timetaken = end - start
+
+            #This tells the user how long it took to find the password as well as how many attempts it took.
+            print("Found it in ", timetaken, " seconds and ", counter, "attempts")
+
+            #This tells the user how many attempst were made per second.
+            print("That is ", counter / timetaken, " attempts per second!")
+
+            #This prints the password to confirm for the user that the program was sucessful.
+            print(i)
+
+            #This stops the program from exiting until the user presses enter.
+            input("Press enter when you have finished")
+
+            #This exits the program
+            exit()
