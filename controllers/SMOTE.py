8ffd2b0d46aa0559e209c2340e05d2b256e59af3
ernestobone98 <ernesto.bone@outlook.com>
2022-04-01 16:07:08 +0200

    ml debug

--- controllers/SMOTE.py
+++ controllers/SMOTE.py
@@ -1,22 +1,24 @@
+from dataclasses import dataclass
 import pandas  as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.utils import resample
+from imblearn.over_sampling import KMeansSMOTE
 from sklearn.svm import SVC
-from sklearn.datasets import make_classification
-from collections import Counter
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
 
 # ---------------------- preprocesing data ----------------------
 data = pd.read_csv('models\\creditcard.csv', sep= ',')
-# data = data.drop(['Time', 'Amount'], axis=1)
 
-X = data.iloc[:, data.columns != 'Class']
-y = data.iloc[:, data.columns == 'Class']
+X = data.iloc[:, data.columns != 'Class'].sample(n=50000, random_state=0)
+y = data.iloc[:, data.columns == 'Class'].sample(n=50000, random_state=0)
 
+# Getting the amounts grouped by frauds or not
 gb = data.groupby('Class').agg(
     transactions=('Class', 'count'),
     total_revenue=('Amount', 'sum'),
@@ -26,11 +28,14 @@ print(gb)
 data = data.drop(['Time', 'Amount'], axis=1)
 
 #taking just a part of data to developpe
-X = X.to_numpy()[:10000]
-y = y.to_numpy()
-y = y.flatten()[:10000]
+# X = X.to_numpy()
+# y = y.to_numpy()
+# y = y.flatten()
+print("dataset loader")
 
-# ---------------------- random data generation ----------------------
+# NOTE: DataFrames are allowed with sklearn
+
+# ---------------------- random data generation ---------------------- (FOR TESTS ONLY)
 # print(y.head)
 # print(X.head)
 # showing original data
@@ -39,38 +44,80 @@ y = y.flatten()[:10000]
 #     n_features=2, n_clusters_per_class=1, n_samples=1000, random_state=10)
 
 
-# ---------------------- Plotting imbalance ---------------------- Problem with the graph
-counter = Counter(y)
-
-plt.subplot(121)
-for label, _ in counter.items():
-	row_ix = np.where(y == label)[0]
-	plt.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
-
-# ---------------------- Splitting the data ----------------------
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)		#Dividing dataset with test_set <- 0.3*data
+print("---------------------- Splitting the data ----------------------")
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)		#Dividing dataset with test_set <- 0.3*data
 # sc = StandardScaler()
 # X_train = sc.fit_transform(X_train)			# all data features except Class
 # X_test = sc.fit_transform(X_test)			# Class feature
 
-# ---------------------- Balancing data with SMOTE ----------------------
-over_sample = SMOTE()
-X_train, y_train = over_sample.fit_resample(X_train,y_train)
+
+# ---------------------- Plotting imbalance ---------------------- Problem with the graph
+
+# Using PCA to have a clear plot 
+# pca = PCA(n_components=3)
+# pca.fit(X_train)
+# data_pca = pca.transform(X_train)
+# data_pca = pd.DataFrame(data_pca)
+# data_pca['Class'] = y_train
+
+# Xax = data_pca.iloc[:,0]
+# Yax = data_pca.iloc[:,1]
+# Zax = data_pca.iloc[:,2]
+
+# cdict = {0:'red',1:'green'}
+# labl = {0:'Not Fraud',1:'Fraud'}
+# marker = {0:'*',1:'o'}
+# alpha = {0:.3, 1:.5}
 
 
-counter_s = Counter(y_train)
-plt.subplot(122)
-for label_s, _ in counter_s.items():
-	row_ix_s = np.where(y_train == label_s)[0]
-	plt.scatter(X_train[row_ix_s, 0], X_train[row_ix_s, 1], label=str(label_s))
+# fig = plt.figure(figsize=(7,5))
+# ax = fig.add_subplot(111, projection='3d')
+# for key, grp in data_pca.groupby(['Class']):
+#     ax.scatter(grp.iloc[:,0], grp.iloc[:,1], grp.iloc[:,2], marker=marker[key], label=labl[key])
 
-plt.savefig('SMOTE.png')
-plt.show()
+# ax.legend()
+# plt.show()
+
+print("---------------------- Balancing Data ----------------------")
+
+df = X_train
+df['Class'] = y_train
+
+notfraud  =df[df.Class == 0]
+fraud = df[df.Class ==1]
+
+no_of_sample = round(notfraud.shape[0] *0.05)
+
+nofraud2 =  resample(notfraud,n_samples =no_of_sample,random_state = 0)
+data_sample = pd.concat([nofraud2,fraud],axis= 0)
+x_sample = data_sample.drop('Class',axis =1)
+y_sample = data_sample.Class
+
+over_sample = RandomOverSampler(random_state=0)
+X_train, y_train = over_sample.fit_resample(x_sample,y_sample)
+
+print(X_train)
+print(y_train)
+
+# pca.fit(X_train)
+# data_pca = pca.transform(X_train)
+# data_pca = pd.DataFrame(data_pca)
+# data_pca['Class'] = y_train
+
+# fig = plt.figure(figsize=(7,5))
+# ax = fig.add_subplot(111, projection='3d')
+# for key, grp in data_pca.groupby(['Class']):
+#     ax.scatter(grp.iloc[:,0], grp.iloc[:,1], grp.iloc[:,2], marker=marker[key], label=labl[key])
+
+# ax.legend()
+
+# # plt.savefig('SMOTE.png')
+# plt.show()
 
 # ---------------------- ML models ----------------------
-
-# KNeighbors Application
-model = KNeighborsClassifier()
+print("training")
+# MLPClassifier Application
+model = MLPClassifier(hidden_layer_sizes=(200,))
 model.fit(X_train, y_train)
 y_predict = model.predict(X_test)
 
@@ -78,9 +125,9 @@ print(accuracy_score(y_test, y_predict))
 print(classification_report(y_test, y_predict))
 
 # SVM Application
-model = SVC()
-model.fit(X_train, y_train)
-y_predict = model.predict(X_test)
+# model = SVC()
+# model.fit(X_train, y_train)
+# y_predict = model.predict(X_test)
 
-print(accuracy_score(y_test, y_predict))
-print(classification_report(y_test, y_predict))
+# print(accuracy_score(y_test, y_predict))
+# print(classification_report(y_test, y_predict))
