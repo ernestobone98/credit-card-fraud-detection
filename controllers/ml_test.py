0dc55a1f0a07c941144c44092763514c6b326bd2
mgazzera <marco.gazzera@etu.univ-cotedazur.fr>
2022-04-07 13:54:14 +0200

    Modification tableau

--- controllers/ml_test.py
+++ controllers/ml_test.py
@@ -1,13 +1,15 @@
 from sklearn.metrics import accuracy_score, classification_report
 from joblib import load
 import pandas as pd
+import os
 
+sep = os.path.sep
 
-data = pd.read_csv('models\\creditcard.csv', sep= ',')
+data = pd.read_csv('models{sep}creditcard.csv', sep= ',')
 data = data.drop(['Time', 'Amount'], axis=1)
 X = data.iloc[:, data.columns != 'Class']
 y = data.iloc[:, data.columns == 'Class']
-mlpc = load("controllers\\MLPC.joblib")
+mlpc = load("controllers{sep}MLPC.joblib")
 
 y_predict = mlpc.predict(X)
 print(accuracy_score(y, y_predict))
