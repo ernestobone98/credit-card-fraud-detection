3574aab76742235cfa3897dd5ffe0040df7a4432
bbernaud <benjamin.bernaud@etu.univ-cotedazur.fr>
2022-04-08 10:06:45 +0200

    fix openning pdf (choosing the file)

--- controllers/open_pdf.py
+++ controllers/open_pdf.py
@@ -3,20 +3,21 @@ from tkinter import filedialog
 from tkPDFViewer import tkPDFViewer as pdf
 import os 
 
-# pip install tkPDFViewer
-# https://www.youtube.com/watch?v=BwFQmGysFNE&t=627s # check the difference
+sep = os.path.sep
 
 win = Tk()
 win.geometry("630x700+400+100")
-win.title("Ouvrir rapports")
+win.title("Open reports")
 win.config(bg = "white")
 
 def browseFiles():
-	filename = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select a pdf file", filetype = (("PDF FILE", ".pdf"), ("All files", ".txt")))
+	filename = filedialog.askopenfilename(initialdir = f'{os.getcwd()}{sep}views{sep}reports', 
+											title = "Select a pdf file", 
+											filetypes = [("PDF FILE", ".pdf")])
 	
 	variable1 = pdf.ShowPdf()
 	#Add your pdf location and width and height.
-	variable2 = variable1.pdf_view(win,pdf_location=r"location",width=50,height=100)
+	variable2 = variable1.pdf_view(win,pdf_location=filename,width=100,height=100)
 	variable2.pack()
 
 Button(win, text="Open", command=browseFiles, width = 40, font ="Arial 20", bd = 4).pack()
