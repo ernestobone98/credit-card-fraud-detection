4ac8b269f10434b8f45bff4c812486b7a18c8a8a
mgazzera <marco.gazzera@etu.univ-cotedazur.fr>
2022-04-15 19:02:52 +0200

    Optimisation open win BBG

--- controllers/open_pdf.py
+++ controllers/open_pdf.py
@@ -6,7 +6,7 @@ import os
 sep = os.path.sep
 
 win = Tk()
-win.geometry("630x700+400+100")
+win.geometry("0x0+800+500")
 win.title("Open reports")
 win.config(bg = "white")
 
@@ -14,12 +14,12 @@ def browseFiles():
 	filename = filedialog.askopenfilename(initialdir = f'{os.getcwd()}{sep}views{sep}reports', 
 											title = "Select a pdf file", 
 											filetypes = [("PDF FILE", ".pdf")])
-	
+
 	variable1 = pdf.ShowPdf()
+	win.geometry('630x700+400+100')
 	#Add your pdf location and width and height.
 	variable2 = variable1.pdf_view(win,pdf_location=filename,width=100,height=100)
 	variable2.pack()
 
-Button(win, text="Open", command=browseFiles, width = 40, font ="Arial 20", bd = 4).pack()
-
+browseFiles()
 win.mainloop()
