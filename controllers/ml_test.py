f085055805e808e24e661140f54a7c0e9544b3c2
ernestobone98 <ernesto.bone@outlook.com>
2022-04-22 10:13:26 +0200

    csv creditcard repaired

--- controllers/ml_test.py
+++ controllers/ml_test.py
@@ -9,9 +9,10 @@ sep = os.path.sep
 current_dir = os.getcwd()
 
 data = pd.read_csv(f'{current_dir}{sep}models{sep}creditcard.csv', sep= ',')
+new_data = data
+data = data.drop(['Time', 'Amount'], axis=1)
 clients = []
 X_exp = data.iloc[:, data.columns != 'Class']
-data = data.drop(['Time', 'Amount', 'ID'], axis=1)
 X = data.iloc[:, data.columns != 'Class']
 y = data.iloc[:, data.columns == 'Class']
 mlpc = load(f'{current_dir}{sep}controllers{sep}MLPC.joblib')
@@ -34,12 +35,15 @@ def main():
 
     print(f)
     print(nf)
-
-    # for i in range(len(data)):
+    print(data.head())
+    # for i in range(len(new_data)):
     #     n = random.randint(1,100)
     #     clients.append(n)
-    # data['ID'] = clients
-    # data.to_csv("models\\creditcard.csv")
+    # new_data['ID'] = clients
+    # new_data = new_data.drop(['Time'], axis=1)
+    # new_data.reset_index(drop=True)
+    # print(new_data.head())
+    # new_data.to_csv("models\\creditcard.csv")
 
     # n est un dataframe qui contient 10 transactions normaux et 3 frauduleusse qui vont etre utiliser comme demo dans l'interface de l'exper
     n = f.append(nf)
