2b58fd355cc9b25c4905c275aa12f8f495cf66a0
mgazzera <marco.gazzera@etu.univ-cotedazur.fr>
2022-04-23 21:57:12 +0200

    close when the message is send

--- controllers/write.py
+++ controllers/write.py
@@ -8,10 +8,13 @@ sep = os.path.sep
 def export():
     with open(f"views{sep}reports{sep}message_patron.txt", 'w') as f:
         f.write(textbox.get("1.0", END).strip())
-
+def close():
+    win.destroy()
+    
 win = Tk()
 win.title("Write your message for the boss")
 textbox = ScrolledText(win)
 textbox.pack(fill=BOTH, expand=YES)
-Button(win, text="Send", command=export).pack(fill=X)
+Button(win, text="Send", command=lambda:[export(),close()]).pack(fill=X)
+
 win.mainloop()
