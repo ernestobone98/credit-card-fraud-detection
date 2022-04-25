2da655e5e424e9638e9c2257228920b483e3ebf3
bbernaud <benjamin@PC-Ben.localdomain>
2022-04-25 13:28:06 +0200

    log message for mail sent

--- controllers/send_mail.py
+++ controllers/send_mail.py
@@ -5,14 +5,15 @@ from subprocess import call
 sep = os.path.sep
 
 victimes = main()
-print(victimes)
+# print(victimes)
 
 ID = list(victimes['ID'])
 Surname = list(victimes[' Prenom'])
 Name = list(victimes[' Nom'])
 Mail = list(victimes[' Adresse mail'])
 
-print(ID, Surname, Name, Mail)
+# print(ID, Surname, Name, Mail)
 
 for prenom, nom, mail, id in zip(Surname, Name, Mail, ID):
     call(['python3', f'controllers{sep}send_email_fraude.py', prenom, nom, mail, str(id)])
+    print(f"Mail envoyé avec succes a {prenom} {nom} ! [{mail}]")
